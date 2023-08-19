4da953aefa1035f4bf4e5e85c2dd9d9fc59d3e07
anagri <127566+anagri@users.noreply.github.com>
2023-08-19 16:49:50 +0530

    [Amir] fixing generation issues, fixed the directory, and other misc items

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,14 +10,17 @@ def remove_file(filepath):
     if os.path.exists(target):
         os.remove(target)
 
+
 def remove_dir(dirname):
     if os.path.exists(dirname):
         shutil.rmtree(dirname)
 
-if __name__ == '__main__':
 
-    if '{{ cookiecutter.create_doc_files }}' != 'y':
-        remove_dir('docs')
+if __name__ == "__main__":
+    if "{{ cookiecutter.create_doc_files }}" != "y":
+        remove_dir("docs")
+        doc_files = ["AUTHORS.rst", "CONTRIBUTING.rst", "HISTORY.rst", "MANIFEST.in"]
+        [remove_file(d) for d in doc_files]
 
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
+    if "Not open source" == "{{ cookiecutter.open_source_license }}":
+        remove_file("LICENSE")
